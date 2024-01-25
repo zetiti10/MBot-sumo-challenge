a4382742ee8d5d4826cd692786baffb7255f7645
Louis L <louis.leculier@gmail.com>
2024-01-25 08:39:59 +0100

    Programme de base

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,9 @@ MeDCMotor rightWheelMotor(PIN_RIGHT_WHEEL_MOTOR);
 MeRGBLed onBoardRGBLED(PIN_RIGHT_ONBOARD_RGB_LED, PIN_LEFT_ONBOARD_RGB_LED);
 MeBuzzer onBoardBuzzer;
 
+// Variables globales.
+int mode = BUTTON_A;
+
 // Cette fonction s'exécute une fois au démarrage du MBot.
 void setup()
 {
@@ -40,4 +43,51 @@ void setup()
 void loop()
 {
     // Programme exécuté en boucle.
+
+    // Sélection du mode.
+    if (onBoardInfraredSensor.keyPressed(BUTTON_A))
+    {
+        mode = BUTTON_B;
+    }
+
+    else if (onBoardInfraredSensor.keyPressed(BUTTON_B))
+    {
+        mode = BUTTON_B;
+    }
+
+    else if (onBoardInfraredSensor.keyPressed(BUTTON_C))
+    {
+        mode = BUTTON_C;
+    }
+
+    else if (onBoardInfraredSensor.keyPressed(BUTTON_D))
+    {
+        mode = BUTTON_D;
+    }
+
+    else if (onBoardInfraredSensor.keyPressed(BUTTON_E))
+    {
+        mode = BUTTON_E;
+    }
+
+    // Exécution du mode en cours.
+    if (mode == BUTTON_A)
+    {
+    }
+
+    else if (mode == BUTTON_B)
+    {
+    }
+
+    else if (mode == BUTTON_C)
+    {
+    }
+
+    else if (mode == BUTTON_D)
+    {
+    }
+
+    else if (mode == BUTTON_E)
+    {
+    }
 }
